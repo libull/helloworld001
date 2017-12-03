b619aefb64892341c4ad5cb7a48ceff19906c81e
libull <libull2008@qq.com>
2017-12-03 17:59:29 +0800

    ss

---
+++ helloworld001/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace helloworld001
+{
+    public class Class2
+    {
+        /// <summary>
+        /// this is a test method
+        /// </summary>
+        /// <param name="arg"></param>
+        public void MyTest(string arg)
+        {
+            Console.WriteLine(arg);
+        }
+    }
+}
