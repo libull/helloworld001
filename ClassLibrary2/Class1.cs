6bbc33891af70898e4b098f23b8e3c85a54c2d00
libull <libull2008@qq.com>
2017-12-03 18:02:56 +0800

    dddd

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public void Ok(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
