d57f66d29315771168c3cec0aa87513e32289844
libull <libull2008@qq.com>
2017-12-03 18:15:47 +0800

    zsdc

---
+++ ClassLibrary2/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class Class2
+    {
+        public void Cry(string why)
+        {
+            Console .WriteLine(why);
+        }
+    }
+}
